a7c7e72f4769dd10a2a355c5b19a69e9e81d8eb6
Mohammed Marjan <mohammedmarjan786@gmail.com>
2025-08-06 15:27:50 +0530

    feat: create asyncHandler

---
+++ utils/asyncHandler.js
@@ -0,0 +1,8 @@
+// Catches errors in async controllers and forwards to Express error handler
+function asyncHandler(fn) {
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
+module.exports = asyncHandler;
