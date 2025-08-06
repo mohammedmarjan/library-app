99c5fc0fe434bbaca8e3e30eb27aeec54e53dbf2
Mohammed Marjan <mohammedmarjan786@gmail.com>
2025-08-06 15:32:31 +0530

    refactor: remove unwanted logs

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,8 +4,6 @@ function errorHandler(err, req, res, next) {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
 
-  console.error(`[ERROR] ${statusCode} - ${message}`);
-
   res.status(statusCode).json({
     success: false,
     error: message,
